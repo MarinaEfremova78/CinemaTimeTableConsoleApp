ed4a8bdccd4c78f158831c4387f82501ba0eb08b
mar.efremova2017@gmail.com <mar.efremova2017@gmail.com>
2021-09-29 19:08:29 +0300

    Add project files.

---
+++ CinemaTimeTableConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Film
+{
+    public static double TotalShowTime { get; set; }
+    public string Title { get; set; }
+    public double Duration { get; set; }
+    public int DailyAmount { get; set; }
+
+    public Film(string title, double duration, int dailyAmount)
+    {
+        Title = title;
+        Duration = duration;
+        DailyAmount = dailyAmount;
+        TotalShowTime += DailyAmount;
+    }
+    public override string ToString()
+    {
+        return $"{Title} {Duration}";
+    }
+}
+
+namespace CinemaTimeTableConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
