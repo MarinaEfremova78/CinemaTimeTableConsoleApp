e9c59952caee210181e4fe6d95dd0f5c08672aef
mar.efremova2017@gmail.com <mar.efremova2017@gmail.com>
2021-09-29 19:10:59 +0300

    Добавила класс Room для создания залов.

--- CinemaTimeTableConsoleApp/Program.cs
+++ CinemaTimeTableConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 public class Film
 {
     public static double TotalShowTime { get; set; }
@@ -16,6 +17,41 @@ public class Film
     public override string ToString()
     {
         return $"{Title} {Duration}";
+    }
+}
+
+public class Room
+{
+    public int Number { get; set; }
+    public double TotalTime { get; private set; }
+    public List<Film> Shows = new List<Film>();
+
+    public Room(int number)
+    {
+        Number = number;
+        TotalTime = 14;
+    }
+
+    public void Append(Film film)
+    {
+        if (film.DailyAmount > 0)
+        {
+            Shows.Add(film);
+            film.DailyAmount--;
+            double middleBreak = 0.15;
+            TotalTime = TotalTime - film.Duration - middleBreak;
+            Film.TotalShowTime--;
+        }
+    }
+
+    public override string ToString()
+    {
+        string shows = "";
+        foreach (var show in Shows)
+        {
+            shows += show + " ";
+        }
+        return $"{Number} {shows} {TotalTime}";
     }
 }
 
